3ecb73b2aeea6e33eb2a8ff100fddd3343c933f7
Abdelrahman Hazin <abooodmh2@gmail.com>
2025-10-27 19:53:58 -0400

    Final updates before merging to main

--- LMS_Sprint1_Team5/src/edu/vsu/lms/model/League.java
+++ LMS_Sprint1_Team5/src/edu/vsu/lms/model/League.java
@@ -1,7 +1,11 @@
 package edu.vsu.lms.model;
 import java.util.HashMap;
 import java.util.Map;
-public class League {
+import java.io.*;
+import java.io.Serializable;
+
+public class League implements Serializable {
+    private static final long serialVersionUID = 1L;
     private final String name;
     private final Map<String, Team> teams = new HashMap<>();
     public League(String name){ this.name = name; }
